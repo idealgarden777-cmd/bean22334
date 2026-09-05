919e613da640da57b9f6e7741f251916e2c785ec
idealgarden777-cmd <idealgarden777@gmail.com>
2026-09-05 09:51:51 +0500

    Update chat-list.js

--- src/components/chat-list.js
+++ src/components/chat-list.js
@@ -1,1 +1,7 @@
-export function renderChatList() { return document.createElement('div'); }
+import { store } from '../core/store.js';
+
+export function renderChatList() {
+  const container = document.createElement('div');
+  container.className = 'chat-list';
+  return container;
+}
